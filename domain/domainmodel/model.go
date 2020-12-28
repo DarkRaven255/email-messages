4a07d2d14bab2336144aab75da91c8578ff099f9
Konrad Dulemba <konrad@dulemba.me>
2020-12-29 00:24:34 +0100

    Model constructor

--- domain/domainmodel/model.go
+++ domain/domainmodel/model.go
@@ -2,18 +2,40 @@ package domainmodel
 
 import (
 	"email-messages/delivery/commands"
+	"errors"
 	"time"
+
+	"github.com/gocql/gocql"
 )
 
 type Model struct {
-	ID          int       `json:"id"`
-	CreatedAt   time.Time `json:"created_at"`
-	Email       string    `json:"email"`
-	Title       string    `json:"title"`
-	Content     string    `json:"content"`
-	MagicNumber int       `json:"magic_number"`
+	Id          gocql.UUID `cql:"id"`
+	Timestamp   time.Time  `cql:"timestamp"`
+	Email       string     `cql:"email"`
+	Title       string     `cql:"title"`
+	Content     string     `cql:"content"`
+	MagicNumber int        `cql:"magic_number"`
 }
 
-func NewDomainModel(cmd *commands.AddMessageCmd) *Model {
-	return nil
+func NewDomainModel(cmd *commands.AddMessageCmd) (*Model, error) {
+
+	if &cmd.Email == nil {
+		return nil, errors.New("Email can not be empty!")
+	}
+	if &cmd.Title == nil {
+		return nil, errors.New("Title can not be empty!")
+	}
+	if &cmd.Content == nil {
+		return nil, errors.New("Content can not be empty!")
+	}
+	if &cmd.MagicNumber == nil {
+		return nil, errors.New("MagicNumber can not be empty!")
+	}
+
+	return &Model{
+		Email:       cmd.Email,
+		Title:       cmd.Title,
+		Content:     cmd.Content,
+		MagicNumber: cmd.MagicNumber,
+	}, nil
 }
