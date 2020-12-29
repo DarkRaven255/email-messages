e46445b8d8bba1c6222cc33b52eae4635a29a2f2
Konrad Dulemba <konrad@dulemba.me>
2020-12-29 02:04:59 +0100

    Update config

--- config/config.go
+++ config/config.go
@@ -3,10 +3,15 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	Port       string
-	DbUsername string
-	DbPassword string
-	DbKeyspace string
+	Port          string
+	DbCluster1    string
+	DbUsername    string
+	DbPassword    string
+	DbKeyspace    string
+	EmailHost     string
+	EmailPort     int
+	EmailUsername string
+	EmailPassword string
 }
 
 var Cfg Config
@@ -15,9 +20,14 @@ func LoadConfig() error {
 	viper.AutomaticEnv()
 
 	Cfg.Port = viper.GetString("PORT")
-	Cfg.DbUsername = viper.GetString("DBUSERNAME")
-	Cfg.DbPassword = viper.GetString("DBPASSWORD")
-	Cfg.DbKeyspace = viper.GetString("DBKEYSPACE")
+	Cfg.DbCluster1 = viper.GetString("DB_CLUSTER1")
+	Cfg.DbUsername = viper.GetString("DB_USERNAME")
+	Cfg.DbPassword = viper.GetString("DB_PASSWORD")
+	Cfg.DbKeyspace = viper.GetString("DB_KEYSPACE")
+	Cfg.EmailHost = viper.GetString("EMAIL_HOST")
+	Cfg.EmailPort = viper.GetInt("EMAIL_PORT")
+	Cfg.EmailUsername = viper.GetString("EMAIL_LOGIN")
+	Cfg.EmailPassword = viper.GetString("EMAIL_PASSWORD")
 
 	return nil
 }
