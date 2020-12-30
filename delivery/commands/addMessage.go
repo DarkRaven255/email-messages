6143acb3e577d9e0a35cf519eef157eb2a66e794
Klaudia Dulemba <klaudiadulemba255@gmail.com>
2020-12-30 16:54:14 +0100

    Enable validator

--- delivery/commands/addMessage.go
+++ delivery/commands/addMessage.go
@@ -1,8 +1,8 @@
 package commands
 
 type AddMessageCmd struct {
-	Email       string `json:"email"`
-	Title       string `json:"title"`
-	Content     string `json:"content"`
-	MagicNumber int    `json:"magic_number"`
+	Email       string `json:"email" validate:"required,email"`
+	Title       string `json:"title" validate:"required"`
+	Content     string `json:"content" validate:"required"`
+	MagicNumber int    `json:"magic_number" validate:"required,numeric"`
 }
